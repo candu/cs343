9575c8511e9ded767372e3195d2ef9f9b72030c2
Fearless Tost <me@fearlesstost.com>
2011-07-19 23:32:51 -0700

    Framework of cardgame for a2q1
    - added basic game flow: stubbed out all Printer, Player methods and
      implemented uMain::main() (including command-line argument parsing.)
      not tested yet.
    - using PRNG with default seed time(NULL) ^ getpid(); this gives
      slightly less predictable behavior than just using time(NULL).
    - started using uint32_t and other _t types; my understanding is that
      these are more portable than int, unsigned int, etc. since their sizes
      are not architecture-dependent.
    - using an interesting idiom i saw once for parsing positional
      command-line arguments, based off switch statement fall-through.
      nothing i'd use for more involved programs, but definitely useful
      here.
    - note that unistd.h is only available on POSIX/Unix-compliant systems.
      not that i'd use anything else, but still...
    - uMain::main() currently allocates an array of pointers to Players on
      the stack, so those pointers have to be manually deleted at the end of
      each game to avoid leaking memory. i'd like to use an array of
      boost::scoped_ptr<Player> instead, but i still have to set that up on
      my system. (fortunately i'm not actually doing this for class, or i
      doubt i'd be allowed to use such sensible things.)

--- a2/q1cardgame.cc
+++ a2/q1cardgame.cc
@@ -1,11 +1,20 @@
 #include <uC++.h>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <unistd.h>
+
+#include "PRNG.h"
+
+using namespace std;
+
+static PRNG gRand;
 
 class Printer {
  public:
-  Printer(const unsigned int NoOfPlayers, const unsigned int NoOfCards);
-  void prt(const unsigned int id,
-           const unsigned int took,
-           unsigned int PlayersRemaining);
+  Printer(const uint32_t NoOfPlayers, const uint32_t NoOfCards) { }
+  void prt(
+      const uint32_t id, const uint32_t took, uint32_t PlayersRemaining) { }
  private:
   void main() {
 
@@ -15,10 +24,10 @@ class Printer {
 _Coroutine Player {
  public:
   enum { DEATH_DECK_DIVISOR = 5 };
-  static void players(unsigned int num);
-  Player(Printer& printer, unsigned int id);
-  void start(Player& lp, Player& rp);
-  void play(unsigned int deck);
+  static void players(uint32_t num) { }
+  Player(Printer& printer, uint32_t id) { }
+  void start(Player& lp, Player& rp) { }
+  void play(uint32_t deck) { }
  private:
   void main() {
 
@@ -26,5 +35,37 @@ _Coroutine Player {
 };
 
 void uMain::main() {
+  uint32_t games = 5;
+  // something suitably random. i saw this used once, and it seemed like a
+  // not overly terrible idea :)
+  uint32_t seed = time(NULL) ^ getpid();
+  // this command-line argument reading idiom is awesome, at least for
+  // quick-and-dirty scripts that can get away with positional arguments.
+  switch (argc) {
+    case 3:
+      seed = atoi(argv[2]);
+    case 2:
+      games = atoi(argv[1]);
+  }
 
+  for (uint32_t g = 0; g < games; g++) {
+    uint32_t numPlayers = gRand(2, 10);
+    uint32_t numCards = gRand(10, 200);
+    uint32_t startingPlayer = gRand(0, numPlayers-1);
+    Player::players(numPlayers);
+    Printer printer(numPlayers, numCards);
+    Player* players[numPlayers];
+    for (uint32_t p = 0; p < numPlayers; p++) {
+      players[p] = new Player(printer, p);
+    }
+    for (uint32_t p = 0; p < numPlayers; p++) {
+      players[p]->start(
+          *players[(p + numPlayers - 1) % numPlayers],
+          *players[(p + 1) % numPlayers]);
+    }
+    players[startingPlayer]->play(numCards);
+    for (uint32_t p  = 0; p < numPlayers; p++) {
+      delete players[p];
+    }
+  }
 }
