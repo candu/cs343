0a32d4fdd90eb4e2a41ad9a53dce78f74002a10b
Fearless Tost <me@fearlesstost.com>
2011-07-25 21:19:39 -0700

    Working implementation of card game from A2Q1
    - slight deviation from the assignment interfaces, because i can :)
      rather than having the Players pass number of cards, etc. into the
      Printer, they just pass a PlayerInfo struct instance that contains
      information on how many cards remain/were taken, whether the player
      died, etc. this makes everything cleaner.
    - i had a bug for a while where execution never truly stopped; to break
      the coroutine cycle, you *have* to exit the last player coroutine.
      this causes the starter (in the case of Players, uMain::main()) to
      resume execution, which in this implementation causes the game to end.
    - there's still a memory leak somewhere; i thought the Players were
      being cleaned up, so maybe something else is leaking. (the Printer,
      perhaps? it does maintain a buffer of PlayerInfo objects, after
      all...)
    - note on C++ enums: to refer to a public nested enum within a class,
      you do *not* need the enum name. in this case, PlayerInfo::PASS_LEFT
      compiles whereas PlayerInfo::Action::PASS_LEFT does not. i guess this
      comes with enums being just fancy wrappers around ints in C++; i'd be
      interested to check out C++0x support for typesafe enums :)
    - i think i'll add -Werror -Wfatal-errors to future compiles, to get
      that nice "fail on warning" behavior; most compiler warnings are in
      fact insidious errors that should be caught. (while on the subject of
      compiler flags, i think i'll see what -j setting results in maximal
      compilation speedup at work...)
    - interesting that operator<<() is not a member function in most
      tutorials...couldn't you just write ostream& operator<<(ostream& out)
      as a member? (ah, but maybe this has to do with operator rewriting
      during precompilation. what about os << a << b? do we understand this
      as operator<<(operator<<(os, a), b) or as
      b.operator<<(a.operator<<(os))? and maybe this has ramifications for
      virtual/overridden functions? i'll see if i can find a better
      explanation somewhere...)

--- a2/q1cardgame.cc
+++ a2/q1cardgame.cc
@@ -10,29 +10,209 @@ using namespace std;
 
 static PRNG gRand;
 
-class Printer {
- public:
-  Printer(const uint32_t NoOfPlayers, const uint32_t NoOfCards) { }
-  void prt(
-      const uint32_t id, const uint32_t took, uint32_t PlayersRemaining) { }
- private:
-  void main() {
+/**
+ * Used by the Printer to store player state information for printing.
+ */
+struct PlayerInfo {
+  enum Action {
+    PASS_LEFT,
+    PASS_RIGHT,
+    END_NO_MORE_CARDS,
+    END_NO_MORE_PLAYERS,
+    NONE
+  };
 
+  PlayerInfo()
+    : cardsTaken_(0),
+      cardsRemaining_(0),
+      action_(NONE),
+      overwroteBuffer_(false),
+      died_(false) { }
+
+  friend ostream& operator<<(ostream& os, const PlayerInfo& info);
+  
+  uint32_t cardsTaken_;
+  uint32_t cardsRemaining_;
+  Action action_;
+  bool overwroteBuffer_;
+  bool died_;
+};
+
+ostream& operator<<(ostream& os, const PlayerInfo& info) {
+  if (info.action_ == PlayerInfo::END_NO_MORE_PLAYERS) {
+    os << "#";
+  } else {
+    os << info.cardsTaken_;
+    os << ":";
+  }
+  os << info.cardsRemaining_;
+  switch (info.action_) {
+    case PlayerInfo::PASS_LEFT:
+      os << "<";
+      break;
+    case PlayerInfo::PASS_RIGHT:
+      os << ">";
+      break;
+    case PlayerInfo::END_NO_MORE_CARDS:
+    case PlayerInfo::END_NO_MORE_PLAYERS:
+      os << "#";
+      break;
+    default:
+      // TODO: raise error
+      break;
+  }
+  if (info.overwroteBuffer_) {
+    os << "*";
+  }
+  if (info.died_) {
+    os << "X";
+  }
+  return os;
+}
+
+_Coroutine Printer {
+ public:
+  Printer(const uint32_t NoOfPlayers, const uint32_t NoOfCards)
+    : playersInitial_(NoOfPlayers),
+      cardsInitial_(NoOfCards) {
+    // assume ids are assigned on [0, playersInitial_)
+    buffer_ = new PlayerInfo*[playersInitial_];
+    // ensure initial state is sensible
+    for (uint32_t i = 0; i < playersInitial_; i++) {
+      buffer_[i] = NULL;
+    }
+    prtHeader();
+  }
+  /**
+   * We're departing from the assignment here because it seems stupid to
+   * duplicate this logic in the Player and Printer, and not much better to
+   * put all playing logic in the Printer - instead, we'll have the Player
+   * decide what PlayerInfo it wants to send to the printer. (The Player is
+   * trusted anyways; we trust it to accurately report cards remaining, etc.)
+   */
+  void prt(
+      const uint32_t id, PlayerInfo* info) {
+    if (buffer_[id] != NULL) {
+      buffer_[id]->overwroteBuffer_ = true;
+      flush();
+    }
+    buffer_[id] = info;
+    if (info->action_ == PlayerInfo::END_NO_MORE_PLAYERS ||
+        info->action_ == PlayerInfo::END_NO_MORE_CARDS) {
+      // we assume Players will trigger suspend() back into uMain::main(),
+      // so that the printer need not really exit
+      flush();
+    }
+    // actual printing takes place in main()
+    resume();
+  }
+ private:
+  uint32_t playersInitial_;
+  uint32_t cardsInitial_;
+  bool done_;
+  PlayerInfo** buffer_;
+  void prtHeader() {
+    cout << "Players: " << playersInitial_ << '\t' <<
+            "Cards: " << cardsInitial_ << endl;
+    for (uint32_t i = 0; i < playersInitial_; i++) {
+      cout << "P" << i << '\t';
+    }
+    cout << endl;
+  }
+  void flush() {
+    for (uint32_t i = 0; i < playersInitial_; i++) {
+      if (buffer_[i] != NULL) {
+        cout << *buffer_[i];
+        delete buffer_[i];
+        buffer_[i] = NULL;
+      }
+      cout << '\t';
+    }
+    cout << endl;
+  }
+  void main() {
+    while (true) {
+      suspend();
+    }
   }
 };
 
 _Coroutine Player {
  public:
   enum { DEATH_DECK_DIVISOR = 5 };
-  static void players(uint32_t num) { }
-  Player(Printer& printer, uint32_t id) { }
-  void start(Player& lp, Player& rp) { }
-  void play(uint32_t deck) { }
+  static void players(uint32_t num) {
+    playersRemaining_ = num;
+  }
+  Player(Printer& printer, uint32_t id)
+    : printer_(printer),
+      id_(id),
+      lp_(NULL),
+      rp_(NULL),
+      deckReceived_(0) { }
+  void start(Player& lp, Player& rp) {
+    lp_ = &lp;
+    rp_ = &rp;
+    resume();
+  }
+  void play(uint32_t deck) {
+    deckReceived_ = deck;
+    // actual playing takes place in main()
+    resume();
+  }
  private:
+  static uint32_t playersRemaining_;
+  Printer& printer_;
+  uint32_t id_;
+  Player* lp_;
+  Player* rp_;
+  uint32_t deckReceived_;
+  void removeSelfFromGame() {
+    lp_->rp_ = rp_;
+    rp_->lp_ = lp_;
+    playersRemaining_--;
+  }
   void main() {
-
+    // start() calls main() to set first starter, so we force a suspend() here
+    // until play() is explicitly called...
+    suspend();
+    while (true) {
+      PlayerInfo* info = new PlayerInfo();
+      if (playersRemaining_ == 1) {
+        info->cardsRemaining_ = deckReceived_;
+        info->action_ = PlayerInfo::END_NO_MORE_PLAYERS;
+      } else {
+        info->cardsTaken_ = min(deckReceived_, gRand(1, 8));
+        info->cardsRemaining_ = deckReceived_ - info->cardsTaken_;
+        if (info->cardsRemaining_ == 0) {
+          info->action_ = PlayerInfo::END_NO_MORE_CARDS;
+        } if (info->cardsRemaining_ % 2 == 0) {
+          info->action_ = PlayerInfo::PASS_RIGHT;
+        } else {
+          info->action_ = PlayerInfo::PASS_LEFT;
+        }
+      }
+      if (deckReceived_ % DEATH_DECK_DIVISOR == 0) {
+        info->died_ = true;
+        removeSelfFromGame();
+      }
+      printer_.prt(id_, info);
+      switch (info->action_) {
+        case PlayerInfo::PASS_LEFT:
+          lp_->play(info->cardsRemaining_);
+          break;
+        case PlayerInfo::PASS_RIGHT:
+          rp_->play(info->cardsRemaining_);
+          break;
+        case PlayerInfo::END_NO_MORE_PLAYERS:
+        case PlayerInfo::END_NO_MORE_CARDS:
+        default:
+          return;
+      }
+    }
   }
 };
+
+uint32_t Player::playersRemaining_ = 0;
 
 void uMain::main() {
   uint32_t games = 5;
@@ -47,6 +227,7 @@ void uMain::main() {
     case 2:
       games = atoi(argv[1]);
   }
+  gRand.seed(seed);
 
   for (uint32_t g = 0; g < games; g++) {
     uint32_t numPlayers = gRand(2, 10);
