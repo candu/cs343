b14b1658bf05c92f7f51179ade03197f55ee4b12
Fearless Tost <me@fearlesstost.com>
2011-07-27 09:18:37 -0700

    Starting work on A2Q3
    - basic implementation of in-place quicksort. it's amazing how rusty i
      am on this stuff; i had to look it up from Wikipedia (although i
      understand perfectly how it works.) there's potential for a terrible
      off-by-one error here (which this implementation fortunately
      misses!); if your two partitions overlap (say, in the middle) you'll
      get awful race conditions on that access. the best way to avoid that
      is to be clear about what low_, high_ mean; here, they mean we are
      sorting array_[low_, high_) (i.e. excluding array_[high_].)
    - this implementation is not actually concurrent, since it waits for the
      subtask to complete sorting the left partition before sorting the
      right partition! to make it concurrent, i'll have to allocate it
      dynamically, delete the subtask in the destructor, and also preserve
      the default "wait for this task to complete" behavior. the "destructor
      waits" idiom is quite clean, though :)
    - a primer on reading from cin. operator>>() skips whitespace, which is
      either really useful or really terrible depending on what you want.
      you can use the noskipws stream manipulator to remove that behavior
      (e.g. cin >> noskipws >> ...) also, eof won't be set until you
      actually read to the end; to trigger this, you need one more read
      after all values have been read. (this is why we check cin.eof() after
      reading the array size!)
    - i'm still not sure how to use the type variable from within the
      program...is this available to the preprocessor, maybe?

--- a2/q3driver.cc
+++ a2/q3driver.cc
@@ -1,3 +1,87 @@
 #include <uC++.h>
+#include <iostream>
 
-void uMain::main() {}
+using namespace std;
+
+template<typename T>
+_Task QuickSort {
+ public:
+  // this task will sort T[low, high)
+  QuickSort(T array[], int low, int high)
+    : array_(array),
+      low_(low),
+      high_(high) { }
+ private:
+  T* array_;
+  int low_;
+  int high_;
+  void swap(int a, int b) {
+    T temp = array_[a];
+    array_[a] = array_[b];
+    array_[b] = temp;
+  }
+  void main() {
+    while (true) {
+      cout << "sorting T[" << low_ << ", " << high_ << ")" << endl;
+      // check for base case
+      int left = low_;
+      int right = high_ - 1;
+      if (left >= right) {
+        break;
+      }
+      // partition around pivot
+      int mid = low_ + (high_ - low_) / 2;
+      T pivot = array_[mid];
+      swap(mid, right);
+      for (int i = left; i < right; i++) {
+        if (array_[i] < pivot) {
+          swap(i, left);
+          left++;
+        }
+      }
+      swap(left, right);
+      mid = left;
+      left = low_;
+
+      // invoke new task to sort left partition
+      // TODO: does this run concurrently?
+      {
+        QuickSort q(array_, left, mid);
+      }
+    
+      // sort right partition
+      low_ = mid;
+    }
+  }
+};
+
+template<typename T>
+void printArray(T array[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void uMain::main() {
+  int n;
+  while (true) {
+    cin >> n;
+    if (cin.eof()) {
+      break;
+    }
+    if (n == 0) {
+      continue;
+    }
+    int* array = new int[n];
+    for (int i = 0; i < n; i++) {
+      cin >> array[i];
+    }
+    printArray(array, n);
+    {
+      QuickSort<int> q(array, 0, n);
+    }
+    printArray(array, n);
+    delete[] array;
+  }
+}
